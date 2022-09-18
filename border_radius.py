8e156371fae87afb6aff96d9d9c05a4b97abc3d8
Alexander Karpinsky <homm86@gmail.com>
2022-09-18 22:52:59 +0300

    Cached mipmap levels

--- border_radius.py
+++ border_radius.py
@@ -1,7 +1,22 @@
 from PIL import Image
 
 
+mask_refs_mitmaps = []
+
+
+def _init_mitmaps():
+    if mask_refs_mitmaps:
+        return
+    ref = Image.open('border_radius.2048.png')
+    mask_refs_mitmaps.insert(0, ref)
+    while ref.width >= 4:
+        ref = ref.resize((ref.width // 2, ref.height // 2), Image.HAMMING)
+        mask_refs_mitmaps.insert(0, ref)
+
+
 def border_radius_mask(size, r_all, r_ne_sw=None, r_se=None, r_sw=None):
+    _init_mitmaps()
+
     if r_ne_sw is None:
         r_ne_sw = r_all
     if r_se is None:
@@ -10,7 +25,6 @@ def border_radius_mask(size, r_all, r_ne_sw=None, r_se=None, r_sw=None):
         r_sw = r_ne_sw
 
     im = Image.new('L', size, 255)
-    ref = Image.open('border_radius.2048.png')
 
     corners_cache = {}
 
@@ -24,7 +38,9 @@ def border_radius_mask(size, r_all, r_ne_sw=None, r_se=None, r_sw=None):
             continue
         corner = corners_cache.get((r, r))
         if corner is None:
-            corner = ref.resize((r, r), Image.HAMMING)
+            mitmap_index = min(r.bit_length(), len(mask_refs_mitmaps) - 1)
+            mask_ref = mask_refs_mitmaps[mitmap_index]
+            corner = mask_ref.resize((r, r), Image.HAMMING)
             corners_cache[(r, r)] = corner
         if transpose is not None:
             corner = corner.transpose(transpose)
