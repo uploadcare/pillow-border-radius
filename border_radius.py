bdc1261491b5cd97ce069763fabf6c9616bf0332
Alexander Karpinsky <homm86@gmail.com>
2022-09-19 12:18:23 +0300

    Change API, get tuples of radii, get vertical radii

--- border_radius.py
+++ border_radius.py
@@ -14,36 +14,58 @@ def _init_mitmaps():
         mask_refs_mitmaps.insert(0, ref)
 
 
-def border_radius_mask(size, r_all, r_ne_sw=None, r_se=None, r_sw=None):
+def border_radius_args(size, radius, vertical_radius=None):
+    if vertical_radius is None:
+        vertical_radius = radius
+
+    def inner(dim, radius):
+        if not isinstance(radius, tuple):
+            radius = (radius,)
+
+        r_nw, r_ne, r_se, r_sw = list(radius) + [None] * (4 - len(radius))
+
+        if r_ne is None:
+            r_ne = r_nw
+        if r_se is None:
+            r_se = r_nw
+        if r_sw is None:
+            r_sw = r_ne
+        return r_nw, r_ne, r_se, r_sw
+
+    return list(zip(
+        inner(size[0], radius),
+        inner(size[1], vertical_radius)
+    ))
+
+
+def border_radius_mask(size, radius, vertical_radius=None):
     _init_mitmaps()
 
-    if r_ne_sw is None:
-        r_ne_sw = r_all
-    if r_se is None:
-        r_se = r_all
-    if r_sw is None:
-        r_sw = r_ne_sw
+    r_nw, r_ne, r_se, r_sw = border_radius_args(size, radius, vertical_radius)
 
     im = Image.new('L', size, 255)
 
     corners_cache = {}
 
     for r, transpose, offset_x, offset_y in [
-        (r_all, None, 0, 0),
-        (r_ne_sw, Image.FLIP_LEFT_RIGHT, 1, 0),
+        (r_nw, None, 0, 0),
+        (r_ne, Image.FLIP_LEFT_RIGHT, 1, 0),
         (r_sw, Image.FLIP_TOP_BOTTOM, 0, 1),
         (r_se, Image.ROTATE_180, 1, 1),
     ]:
-        if r == 0:
+        if min(*r) == 0:
             continue
-        corner = corners_cache.get((r, r))
+        corner = corners_cache.get(r)
         if corner is None:
-            mitmap_index = min(r.bit_length(), len(mask_refs_mitmaps) - 1)
+            mitmap_index = min(max(*r).bit_length(), len(mask_refs_mitmaps) - 1)
             mask_ref = mask_refs_mitmaps[mitmap_index]
-            corner = mask_ref.resize((r, r), Image.HAMMING)
-            corners_cache[(r, r)] = corner
+            corner = mask_ref.resize(r, Image.HAMMING)
+            corners_cache[r] = corner
         if transpose is not None:
             corner = corner.transpose(transpose)
-        im.paste(corner, ((size[0] - r) * offset_x, (size[1] - r) * offset_y))
+        im.paste(corner, (
+            (size[0] - r[0]) * offset_x,
+            (size[1] - r[1]) * offset_y
+        ))
 
     return im
--- test_border_radius.py
+++ test_border_radius.py
@@ -5,22 +5,26 @@ from border_radius import border_radius_mask, Image
 import pytest
 
 
-@pytest.mark.parametrize("size,r_args,r_kwargs", [
-    ((60, 40), (10,), {}),
-    ((60, 40), (10, 12), {}),
-    ((60, 40), (10, 12, 14), {}),
-    ((60, 40), (10, 12, 14, 16), {}),
+@pytest.mark.parametrize("size, radius, vertical_radius", [
+    ((60, 40), (10,), None),
+    ((60, 40), (10, 12), None),
+    ((60, 40), (10, 12, 14), None),
+    ((60, 40), (10, 12, 14, 16), None),
 ])
-def test_referential(size, r_args, r_kwargs):
-    fn_args = []
-    for arg in r_args:
-        fn_args.append(str(arg))
-    for name, arg in r_kwargs.items():
-        fn_args.append(f"{name}={arg}")
-    out_args = ",".join(fn_args)
+def test_referential(size, radius, vertical_radius):
+    def collect_args(radius):
+        fn_args = []
+        for arg in radius:
+            fn_args.append(str(arg))
+        return ",".join(fn_args)
+
+    out_args = collect_args(radius)
+    if vertical_radius is not None:
+        out_args += "_" + collect_args(vertical_radius)
+
     out_file = f"./test_refs/mask_{size[0]}x{size[1]}_{out_args}.png"
 
-    mask = border_radius_mask(size, *r_args, **r_kwargs)
+    mask = border_radius_mask(size, radius, vertical_radius)
 
     if not os.path.exists(out_file):
         mask.save(out_file, optimize=True)
